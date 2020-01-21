a3b67cbcfe08fc9e03032a0d8a7cecdb3ca8f126
jessiexiong <45522871+jessiexiong@users.noreply.github.com>
2020-01-21 17:00:24 -0500

    Update Module2Task4.ino

--- Module2Task4.ino
+++ Module2Task4.ino
@@ -1,3 +1,12 @@
+const int wheel=10;
+int forward = 0;
+int backward = 1;
+int turn90 =2;
+
+//initialize time
+  int volatile time1 = 0;
+  int volatile time2 = 0;
+  
 void setup() {
   // put your setup code here, to run once:
     //initialize pins
@@ -13,28 +22,89 @@ void setup() {
 
     //initialize wheel as input
   pinMode(wheel, INPUT);
-  attachInterrupt(0, forward, CHANGE);
-  attachInterrupt(1, backward, CHANGE);
+  attachInterrupt(2, readspeed1, CHANGE);
+  attachInterrupt(3, readspeed2, CHANGE);
   Serial.begin(9600);
+
+  
 }
 
 void loop() {
-
-    
+ int turnforward = countEncoder(forward, 48);
+   int turnbackward = countEncoder(backward, 32);
+   int turnat90 = countEncoder(turn90, 8);
 }
 
-  void forward ()
+int countEncoder(int direction, int max){
+
+  //if moving forwards
+  if(direction == 0)
   {
+    for(int count=0; count<max; count++)
+    {
+    Serial.println(count);
     digitalWrite(7, HIGH);
-    analogWrite(6, 128);
-    digitalWrite(4, LOW);
-    analogWrite(5, 128);
+    analogWrite(6, 255);
+    digitalWrite(4, HIGH);                                     
+    analogWrite(5, 255);
+    }
+    
   }
 
-  void backward()
+  //if moving backwards
+  else if (direction == 1)
   {
-        digitalWrite(7, LOW);
-    analogWrite(6, 128);
-    digitalWrite(4, HIGH);
-    analogWrite(5, 128);
+    for(int count=0; count< max; count++)
+    {
+    digitalWrite(7, LOW);
+    analogWrite(6, 255);
+    digitalWrite(4, LOW);
+    analogWrite(5, 255);
+    Serial.println(count);
+
+    }
   }
+
+  //if turning 90 degrees
+   else if (direction == 2)
+  {
+    for(int count=0; count< max; count++)
+    {
+    Serial.println(count);
+    digitalWrite(7, LOW);
+    analogWrite(6, 255);
+    
+    digitalWrite(4, HIGH);
+    analogWrite(5, 255);
+    Serial.println(count);
+    }
+  }
+}
+
+//calculate speed of motor 1
+  void readspeed1()
+  {
+    time2 = millis();
+    float t = time2 - time1; //calculate time
+    float d = 0;//calculate distance
+    float s = d/t;//calculate speed
+    Serial.print("Speed of motor 1 is: ");
+    Serial.println(s);
+
+    time1 = millis();
+  }
+  
+//calculate speed of motor 2
+  void readspeed2()
+  {
+    time2 = millis();
+    float t = time2 - time1; //calculate time
+    float d = 0 ;//calculate distance
+    float s = d/t; //calculate speed
+
+    Serial.print("Speed of motor 2 is: ");
+    Serial.println(s);
+
+    time1 = millis();
+  }
+ 
